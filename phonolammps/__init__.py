453099abffc9926b1ebb2ba3599aa1afca9b0267
abel <abelcarreras83@gmail.com>
2019-05-16 11:55:17 +0200

    Add tinker support (only orthorhombic structures)

--- phonolammps/__init__.py
+++ phonolammps/__init__.py
@@ -4,54 +4,21 @@ import numpy as np
 
 from lammps import lammps
 from phonopy.file_IO import write_FORCE_CONSTANTS, write_force_constants_to_hdf5, write_FORCE_SETS
-from phonolammps.arrange import get_correct_arrangement
+from phonolammps.arrange import get_correct_arrangement, rebuild_connectivity_tinker
 from phonolammps.phonopy_link import obtain_phonon_dispersion_bands, get_phonon
-from phonolammps.iofile import get_structure_from_poscar, get_structure_from_lammps, generate_VASP_structure
+from phonolammps.iofile import get_structure_from_poscar, generate_VASP_structure
+from phonolammps.iofile import generate_tinker_key_file, generate_tinker_txyz_file, parse_tinker_forces
+from phonolammps.iofile import get_structure_from_lammps, get_structure_from_txyz
 
 # define the force unit conversion factors to LAMMPS metal style (eV/Angstrom)
 unit_factors = {'real': 4.336410389526464e-2,
                 'metal': 1.0,
-                'si': 624150636.3094}
+                'si': 624150636.3094,
+                'tinker': 0.043  # kcal/mol to eV
+}
 
 
-class Phonolammps:
-    def __init__(self,
-                 lammps_input_file,
-                 supercell_matrix=np.identity(3),
-                 primitive_matrix=np.identity(3),
-                 displacement_distance=0.01,
-                 show_log=False,
-                 show_progress=False,
-                 use_NAC=False,
-                 symmetrize=True):
-        """
-        :param lammps_input_file:  LAMMPS input file name (see example)
-        :param supercell_matrix:  3x3 matrix supercell
-        :param primitive cell:  3x3 matrix primitive cell
-        :param displacement_distance: displacement distance in Angstroms
-        :param show_log: Set true to display lammps log info
-        :param show_progress: Set true to display progress of calculation
-        """
-
-        self._structure = get_structure_from_lammps(lammps_input_file)
-        self._lammps_input_file = lammps_input_file
-
-        self._supercell_matrix = supercell_matrix
-        self._primitive_matrix = primitive_matrix
-        self._displacement_distance = displacement_distance
-        self._show_log = show_log
-        self._show_progress = show_progress
-        self._symmetrize = symmetrize
-        self._NAC = use_NAC
-
-        self._force_constants = None
-        self._data_set = None
-
-        self.units = self.get_units(lammps_input_file)
-
-        if not self.units in unit_factors.keys():
-            print ('Units style not supported, use: {}'.format(unit_factors.keys()))
-            exit()
+class PhonoBase:
 
     def get_units(self, input_file):
         with open(input_file, 'r') as f:
@@ -59,47 +26,6 @@ class Phonolammps:
                 if line.startswith('units'):
                     return line.split()[1]
         return 'lj'
-
-    def get_lammps_forces(self, cell_with_disp):
-        """
-        Calculate the forces of a supercell using lammps
-        :param cell_with_disp: supercell from which determine the forces
-        :return: numpy array matrix with forces of atoms [Natoms x 3]
-        """
-
-        supercell_sizes = np.diag(self._supercell_matrix)
-
-        cmd_list = ['-log', 'none']
-        if not self._show_log:
-            cmd_list += ['-echo', 'none', '-screen', 'none']
-
-        lmp = lammps(cmdargs=cmd_list)
-        lmp.file(self._lammps_input_file)
-        lmp.command('replicate {} {} {}'.format(*supercell_sizes))
-        lmp.command('run 0')
-
-        na = lmp.get_natoms()
-        xc = lmp.gather_atoms("x", 1, 3)
-        reference = np.array([xc[i] for i in range(na * 3)]).reshape((na, 3))
-
-        template = get_correct_arrangement(reference, self._structure, self._supercell_matrix)
-        indexing = np.argsort(template)
-
-        for i in range(na):
-            lmp.command('set atom {} x {} y {} z {}'.format(i + 1,
-                                                            cell_with_disp[template[i], 0],
-                                                            cell_with_disp[template[i], 1],
-                                                            cell_with_disp[template[i], 2]))
-
-        lmp.command('run 0')
-
-        forces = lmp.gather_atoms("f", 1, 3)
-
-        forces = np.array([forces[i] for i in range(na * 3)]).reshape((na, 3))[indexing, :]
-        forces = forces * unit_factors[self.units]
-        lmp.close()
-
-        return forces
 
     def get_path_using_seek_path(self):
 
@@ -151,7 +77,7 @@ class Phonolammps:
             phonon.generate_displacements(distance=self._displacement_distance)
             cells_with_disp = phonon.get_supercells_with_displacements()
 
-            cells_with_disp = [cell.get_positions() for cell in cells_with_disp]
+            #cells_with_disp = [cell.get_positions() for cell in cells_with_disp]
 
             data_set = phonon.get_displacement_dataset()
 
@@ -159,7 +85,7 @@ class Phonolammps:
             for i, cell in enumerate(cells_with_disp):
                 if self._show_progress:
                     print('displacement {} / {}'.format(i+1, len(cells_with_disp)))
-                forces = self.get_lammps_forces(cell)
+                forces = self.get_forces(cell)
                 data_set['first_atoms'][i]['forces'] = forces
 
             phonon.set_displacement_dataset(data_set)
@@ -285,3 +211,251 @@ class Phonolammps:
 
         with open(filename, mode='w') as f:
             f.write(poscar_txt)
+
+################################
+#            LAMMPS            #
+################################
+class Phonolammps(PhonoBase):
+    def __init__(self,
+                 lammps_input_file,
+                 supercell_matrix=np.identity(3),
+                 primitive_matrix=np.identity(3),
+                 displacement_distance=0.01,
+                 show_log=False,
+                 show_progress=False,
+                 use_NAC=False,
+                 symmetrize=True):
+        """
+        :param lammps_input_file:  LAMMPS input file name (see example)
+        :param supercell_matrix:  3x3 matrix supercell
+        :param primitive cell:  3x3 matrix primitive cell
+        :param displacement_distance: displacement distance in Angstroms
+        :param show_log: Set true to display lammps log info
+        :param show_progress: Set true to display progress of calculation
+        """
+
+        self._structure = get_structure_from_lammps(lammps_input_file)
+        self._lammps_input_file = lammps_input_file
+
+        self._supercell_matrix = supercell_matrix
+        self._primitive_matrix = primitive_matrix
+        self._displacement_distance = displacement_distance
+        self._show_log = show_log
+        self._show_progress = show_progress
+        self._symmetrize = symmetrize
+        self._NAC = use_NAC
+
+        self._force_constants = None
+        self._data_set = None
+
+        self.units = self.get_units(lammps_input_file)
+
+        if not self.units in unit_factors.keys():
+            print ('Units style not supported, use: {}'.format(unit_factors.keys()))
+            exit()
+
+    def get_forces(self, cell_with_disp):
+        """
+        Calculate the forces of a supercell using lammps
+        :param cell_with_disp: supercell from which determine the forces
+        :return: numpy array matrix with forces of atoms [Natoms x 3]
+        """
+
+        supercell_sizes = np.diag(self._supercell_matrix)
+
+        cmd_list = ['-log', 'none']
+        if not self._show_log:
+            cmd_list += ['-echo', 'none', '-screen', 'none']
+
+        lmp = lammps(cmdargs=cmd_list)
+        lmp.file(self._lammps_input_file)
+        lmp.command('replicate {} {} {}'.format(*supercell_sizes))
+        lmp.command('run 0')
+
+        na = lmp.get_natoms()
+        xc = lmp.gather_atoms("x", 1, 3)
+        reference = np.array([xc[i] for i in range(na * 3)]).reshape((na, 3))
+
+        template = get_correct_arrangement(reference, self._structure, self._supercell_matrix)
+        indexing = np.argsort(template)
+
+        coordinates = cell_with_disp.get_positions()
+
+        for i in range(na):
+            lmp.command('set atom {} x {} y {} z {}'.format(i + 1,
+                                                            coordinates[template[i], 0],
+                                                            coordinates[template[i], 1],
+                                                            coordinates[template[i], 2]))
+
+        lmp.command('run 0')
+
+        forces = lmp.gather_atoms("f", 1, 3)
+
+        forces = np.array([forces[i] for i in range(na * 3)]).reshape((na, 3))[indexing, :]
+        forces = forces * unit_factors[self.units]
+        lmp.close()
+
+        return forces
+
+################################
+#            TINKER            #
+################################
+class PhonoTinker(PhonoBase):
+
+    def __init__(self,
+                 txyz_input_file,
+                 key_input_file,
+                 force_field_file,
+                 supercell_matrix=np.identity(3),
+                 primitive_matrix=np.identity(3),
+                 displacement_distance=0.01,
+                 show_log=False,
+                 show_progress=False,
+                 use_NAC=False,
+                 symmetrize=True):
+        """
+        :param txyz_input_file:  TXYZ input file name (see example)
+        :param supercell_matrix:  3x3 matrix supercell
+        :param primitive cell:  3x3 matrix primitive cell
+        :param displacement_distance: displacement distance in Angstroms
+        :param show_log: Set true to display lammps log info
+        :param show_progress: Set true to display progress of calculation
+        """
+
+        self._structure = get_structure_from_txyz(txyz_input_file, key_input_file)
+        self._txyz_input_file = txyz_input_file
+
+        self._supercell_matrix = supercell_matrix
+        self._primitive_matrix = primitive_matrix
+        self._displacement_distance = displacement_distance
+        self._show_log = show_log
+        self._show_progress = show_progress
+        self._symmetrize = symmetrize
+        self._NAC = use_NAC
+
+        self._force_constants = None
+        self._data_set = None
+
+        self.units = 'tinker'
+
+        self.force_field = force_field_file
+
+        if not self.units in unit_factors.keys():
+            print ('Units style not supported, use: {}'.format(unit_factors.keys()))
+            exit()
+
+    def get_forces(self, cell_with_disp):
+        """
+        Calculate the forces of a supercell using tinker
+        :param cell_with_disp: supercell (PhonopyAtoms) from which determine the forces
+        :return: numpy array matrix with forces of atoms [Natoms x 3]
+        """
+
+        import tempfile
+        import subprocess
+        import os
+        from subprocess import PIPE
+
+        temp_file_name = tempfile.gettempdir() + '/tinker_temp' + '_' + str(os.getpid())
+
+        # temp_file_name = 'test_calc'
+
+        supercell_wd = rebuild_connectivity_tinker(self._structure,
+                                                   cell_with_disp,
+                                                   self._supercell_matrix)
+
+
+        tinker_input_file = open(temp_file_name + '.txyz', mode='w')
+        tinker_input_file.write(generate_tinker_txyz_file(supercell_wd))
+
+        tinker_key_file = open(temp_file_name + '.key', mode='w')
+        tinker_key_file.write(generate_tinker_key_file(supercell_wd))
+
+        tinker_input_file.close()
+        tinker_key_file.close()
+
+        tinker_command = './testgrad ' + tinker_input_file.name + \
+                         ' ' + self.force_field + ' Y N N ' + ' -k ' + tinker_key_file.name
+
+        tinker_process = subprocess.Popen(tinker_command, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
+        (output, err) = tinker_process.communicate()
+        tinker_process.wait()
+
+        if len(err.split()) != 0:
+            print(err)
+            print('Something wrong in forces calculation!')
+            exit()
+
+        # print(output)
+        os.unlink(tinker_input_file.name)
+        os.unlink(tinker_key_file.name)
+
+        forces = parse_tinker_forces(output) * unit_factors[self.units]
+
+        return forces
+
+
+if __name__ == '__main__':
+
+    structure = get_structure_from_txyz('structure_wrap_min.txyz', 'structure.key')
+    print(structure)
+    print(structure.get_connectivity())
+    print(generate_VASP_structure(structure))
+    print(structure.get_scaled_positions())
+    print(structure.get_chemical_symbols())
+
+    phonon = get_phonon(structure,
+                        setup_forces=False,
+                        super_cell_phonon=[[2, 0, 0], [0, 2, 0], [0, 0, 2]],
+                        #NAC=self._NAC,
+                        symmetrize=True)
+
+
+    phonon.get_displacement_dataset()
+    phonon.generate_displacements(distance=0.0001)
+    cells_with_disp = phonon.get_supercells_with_displacements()
+    print(cells_with_disp[0])
+    print(generate_VASP_structure(cells_with_disp[0]))
+
+    supercell_wd = rebuild_connectivity_tinker(structure,
+                                               cells_with_disp[0],
+                                               phonon.get_supercell_matrix())
+
+    print(generate_tinker_txyz_file(supercell_wd))
+    print(generate_tinker_key_file(supercell_wd))
+
+    print(generate_VASP_structure(structure))
+
+    import tempfile
+    import subprocess
+    import os
+    from subprocess import PIPE
+
+    force_field = 'mm3'
+    temp_file_name = tempfile.gettempdir() + '/tinker_temp'+ '_' + str(os.getpid())
+
+
+    tinker_input_file = open(temp_file_name + '.txyz',mode='w')
+    tinker_input_file.write(generate_tinker_txyz_file(supercell_wd))
+
+    tinker_key_file = open(temp_file_name + '.key',mode='w')
+    tinker_key_file.write(generate_tinker_key_file(supercell_wd))
+
+    tinker_input_file.close()
+    tinker_key_file.close()
+
+
+    print('filename', tinker_input_file)
+    tinker_command = './testgrad ' + tinker_input_file.name + \
+                     ' ' + force_field + ' Y N N ' + ' -k ' + tinker_key_file.name
+
+    tinker_process = subprocess.Popen(tinker_command, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
+    (output, err) = tinker_process.communicate()
+    tinker_process.wait()
+
+    os.unlink(tinker_input_file.name)
+    os.unlink(tinker_key_file.name)
+
+    forces = parse_tinker_forces(output)
+    print(forces)
+    print(forces.shape)
